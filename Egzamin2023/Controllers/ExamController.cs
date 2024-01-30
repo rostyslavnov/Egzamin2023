7fbae506b5b1a9838564f9ba522f6f32f0947015
Cezary Siwoń <siwon.cezary@gmail.com>
2024-01-30 12:25:54 +0100

    Initial commit

---
+++ Egzamin2023/Controllers/ExamController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Egzamin2023.Controllers;
+
+public class ExamController: Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    public IActionResult Update()
+    {
+        return View();
+    }
+}
---
+++ Ezgamin2023Test/Egzamin2023Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Ezgamin2023Test;
+
+public class Egzamin2023Test: IClassFixture<WebApplicationFactory<Program>>
+{
+    
+    private readonly WebApplicationFactory<Program> _factory;
+    private readonly HttpClient _client;
+    private int _points;
+
+    public Egzamin2023Test(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+        _client = _factory.CreateClient();
+    }
+    
+    [Theory]
+    [InlineData("/exam/index")]
+    [InlineData("/exam/create")]
+    [InlineData("/exam/update")]
+    [InlineData("/exam/delete")]
+    public async void Test01(string path)
+    {
+        //Arrange
+        
+        //Act
+        var result = await _client.GetAsync(path);
+    
+        //Assert
+        //string content = await result.Content.ReadAsStringAsync();
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+    }
+    
+    [Fact]
+    public async void Test02()
+    {
+        var result = await _client.GetAsync($"/exam/index");
+        //Assert
+        string content = await result.Content.ReadAsStringAsync();
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        Assert.Contains("<h2>Exam Index</h2>", content);
+    }
+}
