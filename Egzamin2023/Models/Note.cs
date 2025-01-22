4edec8430dcc6ae1abfc660272855c38f0718805
rostyslave <rostyslavenovytskyi@microsoft.wsei.edu.pl>
2025-01-22 02:22:21 +0100

    start

---
+++ Egzamin2023/Models/Note.cs
@@ -0,0 +1,21 @@
+﻿// Novytskyi Rostyslav 15204
+using System.ComponentModel.DataAnnotations;
+
+namespace Egzamin2023.Models
+{
+    public class Note
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 20 characters.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(2000, MinimumLength = 10, ErrorMessage = "Content must be between 10 and 2000 characters.")]
+        public string Content { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Deadline")]
+        public DateTime Deadline { get; set; }
+    }
+}
